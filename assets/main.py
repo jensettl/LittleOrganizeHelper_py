34ae81a416ed54dbacbcc0c5d3a99b25804b8b54
Jens Ettl <ettljens@gmail.com>
2023-09-25 13:25:27 +0200

    added option to automatically sort files

--- assets/main.py
+++ assets/main.py
@@ -10,19 +10,41 @@ path = pathlib.Path.home() / "Downloads"
 fileTypes = list(fileFormat.keys())
 fileFormats = list(fileFormat.values())
 
-def manual_cleanup():
-    if not os.path.exists(path) or not os.path.isdir(path):
-        print("Path is invalid")
-        return
-        
-    for file in os.scandir(path):
-        if not file.is_file():
-            continue
-        
-        fileName = file.name
-        fileFormatType = "." + fileName.split(".")[-1]
-        
-        print(f"File name: {fileName} \nFile path: {path} \nFile size: {file.stat().st_size} bytes")
-        print(fileFormatType)
-        
-manual_cleanup()
+def main():
+	if not os.path.exists(path) or not os.path.isdir(path):
+		print("Path is invalid")
+		return
+	
+	setting = input("Do you want to automatically sort your files? (y/n) > ")
+	
+	if setting.lower() == "y":
+		for file in os.scandir(path):
+			if not file.is_file():
+				continue
+				
+			fileName = file.name
+			fileFormatType = "." + fileName.split(".")[-1]
+			
+			if setting == "y":
+				for i in range(len(fileFormats)):
+					if fileFormatType in fileFormats[i]:
+						print(f"...File {fileName} moved to {fileTypes[i]} folder")
+						folder = path / fileTypes[i]
+						if not os.path.exists(folder):
+							os.mkdir(folder)
+						os.rename(path / fileName, folder / fileName)
+						break
+				else:
+					print(f"No file format found for {fileName}")
+					folder = path  / "Other"
+					if not os.path.exists(folder):
+							os.mkdir(folder)
+					os.rename(path / fileName, folder / fileName)
+					break
+
+	elif setting == "n":
+		# your code for manual cleanup goes here
+		pass
+
+if __name__ == "__main__":
+	main()
