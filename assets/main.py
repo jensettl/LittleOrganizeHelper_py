42227789d42d32f1f52ce0921478d0b787d4510a
Jens Ettl <ettljens@gmail.com>
2023-09-25 17:58:50 +0200

    added manual decisions and improvements
    *logging
    *Comments
    *Types

--- assets/main.py
+++ assets/main.py
@@ -1,50 +1,91 @@
 import os
-import pathlib
+import logging
+from pathlib import Path
+from ressources import FILE_FORMAT_FOLDERS
 
-# import file format dictionary from ressources.py
-from ressources import fileFormat
+# path to downloads folder of current OS user
+path: Path = Path.home() / "Downloads"
+fileFormats = list(FILE_FORMAT_FOLDERS.keys())
+fileTypes = list(FILE_FORMAT_FOLDERS.values())
 
-# path to dowloads folder of current OS user
-path = pathlib.Path.home() / "Downloads"
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("logs/file_sorter.log"), logging.StreamHandler()],
+)
 
-fileTypes = list(fileFormat.keys())
-fileFormats = list(fileFormat.values())
+
+def autoSort(file: Path) -> None:
+    """Automatically sorts a file into the appropriate folder based on its file format."""
+    file_format_type: str = file.suffix.lower()
+
+    if file_format_type in FILE_FORMAT_FOLDERS:
+        folder_name: str = FILE_FORMAT_FOLDERS[file_format_type]
+        folder_path: Path = file.parent / folder_name
+
+        if not folder_path.exists():
+            folder_path.mkdir()
+
+        logging.info(f"Moving {file.name} to {folder_name} folder")
+        file.rename(folder_path / file.name)
+    else:
+        logging.info(f"No file format found for {file.name}. Moving to 'Other' folder")
+        other_folder_path: Path = file.parent / "Other"
+
+        if not other_folder_path.exists():
+            other_folder_path.mkdir()
+
+        file.rename(other_folder_path / file.name)
+
 
 def main():
-	if not os.path.exists(path) or not os.path.isdir(path):
-		print("Path is invalid")
-		return
-	
-	setting = input("Do you want to automatically sort your files? (y/n) > ")
-	
-	if setting.lower() == "y":
-		for file in os.scandir(path):
-			if not file.is_file():
-				continue
-				
-			fileName = file.name
-			fileFormatType = "." + fileName.split(".")[-1]
-			
-			if setting == "y":
-				for i in range(len(fileFormats)):
-					if fileFormatType in fileFormats[i]:
-						print(f"...File {fileName} moved to {fileTypes[i]} folder")
-						folder = path / fileTypes[i]
-						if not os.path.exists(folder):
-							os.mkdir(folder)
-						os.rename(path / fileName, folder / fileName)
-						break
-				else:
-					print(f"No file format found for {fileName}")
-					folder = path  / "Other"
-					if not os.path.exists(folder):
-							os.mkdir(folder)
-					os.rename(path / fileName, folder / fileName)
-					break
+    if not path.exists() or not path.is_dir():
+        logging.error("Path is invalid")
+        return
 
-	elif setting == "n":
-		# your code for manual cleanup goes here
-		pass
+    setting = input("Do you want to automatically sort your files? (Y/n) > ")
+
+    # Automatically sort files
+    if setting.lower() == "y":
+        # Iterate through files and automatically move them to the right folder
+        for file in path.iterdir():
+            if not file.is_file():
+                continue
+
+            autoSort(file)
+
+    # Manually sort files
+    elif setting.lower() == "n":
+        logging.info("Start iterating manually")
+
+        for file in path.iterdir():
+            if not file.is_file():
+                continue
+
+            print(
+                f"File: {file.name} | File format: {file.suffix} | File size: {file.stat().st_size} bytes"
+            )
+
+            action = input(
+                "What do you want to do with this file? (1) Move automatically, (2) Delete or (3) Skip) > "
+            )
+            print("")
+
+            if action == "1":
+                autoSort(file)
+            elif action == "2":
+                logging.info(f"Deleted {file.name} from {file.parent} folder")
+                print(f"...File {file} deleted\n")
+                os.remove(path / file)
+            elif action == "3":
+                logging.info(f"Skipped {file.name} from {file.parent} folder")
+                print(f"...File {file} skipped\n")
+                continue
+            else:
+                logging.error(f"Invalid Input: {action}")
+                break
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- assets/ressources.py
+++ assets/ressources.py
@@ -1,27 +1,19 @@
-# File format dictionary by https://github.com/RiturajSaha/File-Organizer
-fileFormat = {
-	            "Web": [".html5", ".html", ".htm", ".xhtml"], 
-	
-             "Picture": [".jpeg", ".jpg", ".tiff", ".gif",
-                           ".bmp", ".png", ".bpg", "svg",".heif", ".psd"], 
-	
-             "Video": [".avi", ".mkv",".flv", ".wmv",
-                         ".mov", ".mp4", ".webm", ".vob", 
-                         ".mng",".qt", ".mpg", ".mpeg", ".3gp"], 
-	
-             "Document": [".oxps", ".epub", ".pages", ".docx",
-                           ".txt", ".pdf", ".doc", ".fdf",
-                           ".ods",".odt", ".pwi", ".xsn",
-                           ".xps", ".dotx", ".docm", ".dox",
-                           ".rvg", ".rtf", ".rtfd", ".wpd", 
-                           ".xls", ".xlsx", ".ppt","pptx"], 
-	
-              "Compressed": [".a", ".ar", ".cpio", ".iso", 
-                              ".tar", ".gz", ".rz", ".7z",
-                              ".dmg", ".rar", ".xar", ".zip"], 
-	
-              "Audio": [".aac", ".aa", ".aac", ".dvf",
-                         ".m4a", ".m4b", ".m4p", ".mp3",
-                         ".msv", "ogg", "oga", ".raw", 
-                         ".vox", ".wav", ".wma"],
-              } 
+from typing import Dict
+
+FILE_FORMAT_FOLDERS: Dict[str, str] = {
+    ".txt": "Text",
+    ".doc": "Documents",
+    ".docx": "Documents",
+    ".pdf": "Documents",
+    ".ppt": "Documents",
+    ".jpg": "Images",
+    ".jpeg": "Images",
+    ".png": "Images",
+    ".gif": "Images",
+    ".mp3": "Music",
+    ".wav": "Music",
+    ".mp4": "Videos",
+    ".avi": "Videos",
+    ".mov": "Videos",
+    ".wmv": "Videos",
+}
