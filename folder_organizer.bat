8c31f0517ab67ecdd4912736757814514b99a714
Jens Ettl <ettljens@gmail.com>
2024-08-13 22:04:02 +0200

    adjust file name change

--- folder_organizer.bat
+++ folder_organizer.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:/Users/Jens/anaconda3/python.exe" "C:/Users/Jens/Documents/GitHub/LittleOrganizeHelper_py/main.py"
+"C:/Users/Jens/anaconda3/python.exe" "C:/Users/Jens/Documents/GitHub/LittleOrganizeHelper_py/folderOrganizer.py"
 pause
