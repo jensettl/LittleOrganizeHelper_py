32942c0dc03600eb1a0b40b316e5214c0eec426a
Jens Ettl <ettljens@gmail.com>
2024-09-15 18:11:46 +0200

    add: dynamic python path

--- writePathToBat.py
+++ writePathToBat.py
@@ -4,14 +4,18 @@ with open("folder_organizer.bat", "w") as f:
     # get current working directory and write it to the bat file
     current_working_directory = os.getcwd()
     
+    # python installation path
+    python_path = "C:/Users/Jens/anaconda3/python.exe"
+    
     # check if its a windows system
     if os.name == "nt":
         current_working_directory = current_working_directory.replace("\\", "/")
+        python_path = python_path.replace("\\", "/")
     
     f.write(
         f"""
 @echo off
-"C:/Users/Jens/anaconda3/python.exe" "{current_working_directory}/folderOrganizer.py"
+"{python_path}" "{current_working_directory}/folderOrganizer.py"
 pause
 """
     )
